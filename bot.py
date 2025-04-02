b94e7ca2a8b944d7fbfd6da756e0256861bd4487
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-02 12:39:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,22 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | @NT_BOTS_SUPPORT | LISA-KOREA/UPLOADER-BOT-V4
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UPLOADER-BOT-V4
+
+
 import os
-from flask import Flask, request
 from plugins.config import Config
 from pyrogram import Client
 
-app = Flask(__name__)
-
-if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-    os.makedirs(Config.DOWNLOAD_LOCATION)
-
-plugins = dict(root="plugins")
-Client = Client("@UploaderXNTBot",
+if __name__ == "__main__" :
+    
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(root="plugins")
+    Client = Client("@UploaderXNTBot",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     sleep_threshold=300,
-    plugins=plugins
-)
-
-@app.route("/", methods=["POST"])
-def webhook():
-    update = request.get_json()
-    if update:
-        bot.process_update(update)
-    return "OK", 200
-
-if __name__ == "__main__":
-    Client.start()
+    plugins=plugins)
     print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
-    app.run(host="0.0.0.0", port=443)
+    Client.run()

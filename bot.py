9fec1525c6901ce353da18aab86b2eca26bd994e
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-02 12:28:01 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | @NT_BOTS_SUPPORT | LISA-KOREA/UPLOADER-BOT-V4
-
-# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UPLOADER-BOT-V4
-
-
 import os
+from flask import Flask, request
 from plugins.config import Config
 from pyrogram import Client
 
-if __name__ == "__main__" :
-    
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins")
-    Client = Client("@UploaderXNTBot",
+app = Flask(__name__)
+
+if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+    os.makedirs(Config.DOWNLOAD_LOCATION)
+
+plugins = dict(root="plugins")
+Client = Client("@UploaderXNTBot",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     sleep_threshold=300,
-    plugins=plugins)
+    plugins=plugins
+)
+
+@app.route("/", methods=["POST"])
+def webhook():
+    update = request.get_json()
+    if update:
+        bot.process_update(update)
+    return "OK", 200
+
+if __name__ == "__main__":
+    Client.run()
     print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
-    Client.run(webhook_url="https://feminist-nicola-farsa-0a3acaf4.koyeb.app/", port=443)
+    app.run(host="0.0.0.0", port=443)

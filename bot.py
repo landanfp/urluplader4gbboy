9588318d0450f90a36cf8ce67f99e6289836b573
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-07 10:47:34 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | @NT_BOTS_SUPPORT | LISA-KOREA/UPLOADER-BOT-V4
-
-# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UPLOADER-BOT-V4
-
 import os
 import threading
 from plugins.config import Config
@@ -17,22 +13,29 @@ def health_check():
     return {"status": "ok"}
 
 def run_server():
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
+    # اجرای سرور FastAPI در پورت 8000 برای Health Check
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
 
 if __name__ == "__main__":
     # اجرای سرور در یک ترد جداگانه
     threading.Thread(target=run_server, daemon=True).start()
     
+    # بررسی و ساخت پوشه دانلود در صورت نیاز
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     
     plugins = dict(root="plugins")
-    Client = Client("@UploaderXNTBot",
+    
+    # تعریف کلاینت Pyrogram
+    client = Client("@UploaderXNTBot",
                     bot_token=Config.BOT_TOKEN,
                     api_id=Config.API_ID,
                     api_hash=Config.API_HASH,
                     sleep_threshold=300,
                     plugins=plugins)
     
+    # نمایش پیغام
     print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
-    Client.run()
+    
+    # اجرای Pyrogram Client
+    client.run()
